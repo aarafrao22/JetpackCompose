6f89d570800cb78902f6c5aa86893e9e4fa0c23f
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-03-30 23:34:56 +0500

    v1

--- app/src/main/java/com/aarafrao/compose/MainActivity.kt
+++ app/src/main/java/com/aarafrao/compose/MainActivity.kt
@@ -3,33 +3,53 @@ package com.aarafrao.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aarafrao.compose.ui.theme.ComposeTheme
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "hey")
+            mainActivity("hey")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun mainActivity(s: String) {
+    Surface(color = Color.LightGray){
+        Text(
+            text = "Hello"  ,
+            style = TextStyle(
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center),
+            modifier = androidx.compose.ui.Modifier.padding(16.dp)
+                .border(2.dp, MaterialTheme.colors.secondary)
+                .background(MaterialTheme.colors.primary)
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTheme {
-        Greeting("hey")
+        mainActivity("hey")
     }
 }
