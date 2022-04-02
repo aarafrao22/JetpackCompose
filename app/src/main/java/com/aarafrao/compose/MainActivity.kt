3d9a1521ae8f9965790d5a0b635c4fe67e9cf95d
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-04-02 19:11:25 +0500

    v1

--- app/src/main/java/com/aarafrao/compose/MainActivity.kt
+++ app/src/main/java/com/aarafrao/compose/MainActivity.kt
@@ -3,53 +3,79 @@ package com.aarafrao.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aarafrao.compose.ui.theme.ComposeTheme
-import java.lang.reflect.Modifier
+import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            mainActivity("hey")
+            MessageCard(msg = Message("Authoer", "This is Mr. Aaraf Rao here"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun mainActivity(s: String) {
-    Surface(color = Color.LightGray){
-        Text(
-            text = "Hello"  ,
-            style = TextStyle(
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center),
-            modifier = androidx.compose.ui.Modifier.padding(16.dp)
-                .border(2.dp, MaterialTheme.colors.secondary)
-                .background(MaterialTheme.colors.primary)
-                .padding(16.dp)
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colors.secondary, CircleShape)
         )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(2.dp))
+
+        Column {
+            Text(
+                text = msg.author ,
+                color = MaterialTheme . colors . secondaryVariant,
+                fontWeight = FontWeight.Bold
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium)
+            Text(text = msg.body)
+        }
     }
 }
 
-@Preview(showBackground = true)
+
+@Preview
 @Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        mainActivity("hey")
-    }
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message(
+            "Hey, take a look at Jetpack Compose",
+            "Android"
+        )
+    )
 }
+
